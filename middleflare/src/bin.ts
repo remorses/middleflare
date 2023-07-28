29bcac3a8cccff5bc9df10dcf553ae299933073d
remorses <beats.by.morse@gmail.com>
2023-07-28 13:26:36 +0200

    nn

--- middleflare/src/bin.ts
+++ middleflare/src/bin.ts
@@ -6,12 +6,12 @@ const cli = cac(name)
 
 cli.command('')
     .option('--url <url>', 'Your deployed Next.js url, like xxx.fly.dev', {})
+    .option('--middleware <middlewarePath>', 'Your Next.js middleware path', {})
     .option(
         '--use-secrets',
         'process.env.VAR is converted to use Cloudflare secrets',
         { default: false },
     )
-    .option('--middleware <middlewarePath>', 'Your Next.js middleware path', {})
     .action(async (opts) => {
         // console.log(opts)
         const { middleware, url, useSecrets } = opts
